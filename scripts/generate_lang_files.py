34588f93a6d78bd165922995e6ff938bcbf889ae
PINPAL <18197217+PINPAL@users.noreply.github.com>
2025-05-02 15:40:33 +0100

    Fix scripts creating 'blank' lang files

--- scripts/generate_lang_files.py
+++ scripts/generate_lang_files.py
@@ -10,18 +10,27 @@ script_dir = os.path.dirname(__file__)
 #    "Arabic": "ar_sa",
 #    "Asturian": "ast_es",
 #    ...
-
 json_file = os.path.join(script_dir, 'language.json')
 with open(json_file, 'r') as f:
     data = json.load(f)
     
-# Get the en_us.json file from the resource pack
-en_us_file = os.path.join(script_dir, '..', 'assets', 'minecraft', 'lang', 'en_us.json')
+# Yeet existing files in the lang folder
+lang_folder = os.path.join(script_dir, '..', 'assets', 'minecraft', 'lang')
+for filename in os.listdir(lang_folder):
+    if filename.endswith('.json') and filename != 'en_us.json':
+        os.remove(os.path.join(lang_folder, filename))
+    
+# Get the en_us.json file from the resource pack and store its content
+en_us_file = os.path.join(lang_folder, 'en_us.json')
+with open(en_us_file, 'r') as f:
+    en_us_data = json.load(f)
 
-# Create a copy of the en_us.json file for each language and name it according to the language code
+# Loop through all languages in the language.json file
 for language, code in data.items():
-    new_file = os.path.join(script_dir, '..', 'assets', 'minecraft', 'lang', f'{code}.json')
-    with open(en_us_file, 'r') as f:
-        en_us_data = json.load(f)
+    # Skip "en_us" as it already exists
+    if code == 'en_us':
+        continue
+    # Create a copy of the en_us.json file for each language and name it according to the language code
+    new_file = os.path.join(lang_folder, f'{code}.json')
     with open(new_file, 'w') as f:
         json.dump(en_us_data, f, indent=4)
